a6d00ff7c0baccc4eaabe7d86219a9c3bed7eb1d
Marco Au <marco.au.321@gmail.com>
2014-04-19 10:36:12 -0700

    Implemented a fast Scrabble algorithm; functionable up to 25 characters now.

--- src/wordtree.js
+++ src/wordtree.js
@@ -57,12 +57,30 @@ var PrefixTree = function(word){
     return tree.isWord;
   };
 
+  this.hasNode = function(word){
+    var search = function(word, tree){
+      for(var i = 0; i < tree.children.length; i++){
+        if(tree.children[i].value === word){
+          return i;
+        }
+      }
+      return false;
+    };
+    //
+    var tree = this;
+    for(var j = 0; j < word.length; j++){
+      tree = tree.children[search(word.slice(0,j+1), tree)];
+      if(!tree){return false;}
+    }
+    return true;
+  };
+
   this.allWordsBelow = function(word, levels){
     var node = this._getNode(word);
     var words = [];
     var AddAllWords = function(node, arr){
-      if(node !== undefined){
-        //undefined is the result for nod e in ._getNode if no such node
+      if(node !== 'no node'){
+        //'no node' is the result for nod e in ._getNode if no such node
         if(node.isWord){
           arr.push(node.value);
         }
@@ -89,12 +107,36 @@ var PrefixTree = function(word){
     var tree = this;
     for(var j = 0; j < word.length; j++){
       tree = tree.children[search(word.slice(0,j+1), tree)];
-      if(!tree){return undefined;}
+      if(!tree){return 'no node';}
     }
     return tree;
   };
 };
 
+var scrabbleTree = function(word, tree){
+  var goodWords = [];
+  var count = 0;
+  var chArray = word.split('');
+  var checkTree = function(word, arr, tree){
+    count++;
+    console.log('checking the word: ' + word);
+    if(tree.hasNode(word)){
+      if(tree.hasWord(word) && goodWords.indexOf(word) === -1){
+        goodWords.push(word);
+      }
+      for(var i = 0; i < arr.length; i++){
+        var newArr = arr.slice();
+        var newWord = word + newArr.splice(i, 1);
+        checkTree(newWord, newArr, tree);
+      }
+    }
+  };
+  checkTree('', chArray, tree);
+  console.log(count);
+  return goodWords;
+};
+
+/*
 var scrabbleSolution = function(word, tree){
   var goodWords = [];
   var factorial = function(num){
@@ -143,4 +185,4 @@ var scrabbleSolution = function(word, tree){
     }
   }
   return goodWords;
-};
+};*/
